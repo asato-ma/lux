ebc3f2bac9c3aa99a5588c0024531f590f80962d
Mariusz <asato-ma@o2.pl>
2018-07-24 15:22:07 +0200

    Update lux_pl.ts

--- src/qt/locale/lux_pl.ts
+++ src/qt/locale/lux_pl.ts
@@ -1,67 +1,67 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en">
+<TS version="2.1" language="pl">
 <context>
     <name>AddressBookPage</name>
     <message>
         <location filename="../forms/addressbookpage.ui" line="+67"/>
         <source>Right-click to edit address or label</source>
-        <translation>Right-click to edit address or label</translation>
+        <translation>Naciśnij prawy przycisk aby edytować adres lub etykietę</translation>
     </message>
     <message>
         <location line="+27"/>
         <source>Create a new address</source>
-        <translation>Create a new address</translation>
+        <translation>Dodaj nowy adres</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>&amp;New</source>
-        <translation>&amp;New</translation>
+        <translation>&amp;Nowy</translation>
     </message>
     <message>
         <location line="+14"/>
         <source>Copy the currently selected address to the system clipboard</source>
-        <translation>Copy the currently selected address to the system clipboard</translation>
+        <translation>Skopiuj adres do schowka</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>&amp;Copy</source>
-        <translation>&amp;Copy</translation>
+        <translation>&amp;Kopiuj</translation>
     </message>
     <message>
         <location line="+14"/>
         <source>Delete the currently selected address from the list</source>
-        <translation>Delete the currently selected address from the list</translation>
+        <translation>Skasuj wybrany adres z listy</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>&amp;Delete</source>
-        <translation>&amp;Delete</translation>
+        <translation>&amp;Skasuj</translation>
     </message>
     <message>
         <location line="+27"/>
         <source>Export the data in the current tab to a file</source>
-        <translation>Export the data in the current tab to a file</translation>
+        <translation>Eksportuj dane z aktualnej zakładki do pliku</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>&amp;Export</source>
-        <translation>&amp;Export</translation>
+        <translation>&amp;Eksportuj</translation>
     </message>
     <message>
         <location line="+20"/>
         <source>C&amp;lose</source>
-        <translation>C&amp;lose</translation>
+        <translation>C&amp;strata</translation>
     </message>
     <message>
         <location filename="../addressbookpage.cpp" line="+44"/>
         <source>Choose the address to send coins to</source>
-        <translation>Choose the address to send coins to</translation>
+        <translation>Wybierz adres, na który chcesz wysłasz</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>Choose the address to receive coins with</source>
-        <translation>Choose the address to receive coins with</translation>
+        <translation>Wybierz adres, na który chcesz otrzymać</translation>
     </message>
     <message>
         <location line="+6"/>
@@ -71,12 +71,12 @@
     <message>
         <location line="+6"/>
         <source>Sending addresses</source>
-        <translation>Sending addresses</translation>
+        <translation>Adres wysyłający</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>Receiving addresses</source>
-        <translation>Receiving addresses</translation>
+        <translation>Adres otrzymujący</translation>
     </message>
     <message>
         <location line="+7"/>
@@ -85,43 +85,43 @@
     </message>
     <message>
         <location line="+4"/>
-        <source>These are your LUX addresses for receiving payments. It is recommended to use a new receiving address for each transaction.</source>
+        <source>To są Twoje adresy LUX do wysyłania płatności. Zawsze sprawdź kwotę i adres odbiorcy przed wysłaniem.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location line="+6"/>
         <source>&amp;Copy Address</source>
-        <translation>&amp;Copy Address</translation>
+        <translation>&amp;Kopiuj adres</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Copy &amp;Label</source>
-        <translation>Copy &amp;Label</translation>
+        <translation>Copy &amp;Etykieta</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>&amp;Edit</source>
-        <translation>&amp;Edit</translation>
+        <translation>&amp;Edytuj</translation>
     </message>
     <message>
         <location line="+187"/>
         <source>Export Address List</source>
-        <translation>Export Address List</translation>
+        <translation>Eksportuj listę adresów</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Comma separated file (*.csv)</source>
-        <translation>Comma separated file (*.csv)</translation>
+        <translation>Plik z przecinkami oddzielającymi (*.csv)</translation>
     </message>
     <message>
         <location line="+13"/>
         <source>Exporting Failed</source>
-        <translation>Exporting Failed</translation>
+        <translation>Eksport nie powiódł się</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>There was an error trying to save the address list to %1. Please try again.</source>
-        <translation>There was an error trying to save the address list to %1. Please try again.</translation>
+        <translation>Wystąpił błąd podczas zapisywania listy adresów do %1. Spróbuj ponownie.</translation>
     </message>
 </context>
 <context>
@@ -129,17 +129,17 @@
     <message>
         <location filename="../addresstablemodel.cpp" line="+157"/>
         <source>Label</source>
-        <translation>Label</translation>
+        <translation>Etykieta</translation>
     </message>
     <message>
         <location line="+0"/>
         <source>Address</source>
-        <translation>Address</translation>
+        <translation>Adres</translation>
     </message>
     <message>
         <location line="+33"/>
         <source>(no label)</source>
-        <translation>(no label)</translation>
+        <translation>(brak etykiety)</translation>
     </message>
 </context>
 <context>
@@ -147,32 +147,32 @@
     <message>
         <location filename="../forms/askpassphrasedialog.ui" line="+26"/>
         <source>Passphrase Dialog</source>
-        <translation>Passphrase Dialog</translation>
+        <translation>Hasło rozmowy</translation>
     </message>
     <message>
         <location line="+30"/>
         <source>Enter passphrase</source>
-        <translation>Enter passphrase</translation>
+        <translation>Wprowadź hasło</translation>
     </message>
     <message>
         <location line="+14"/>
         <source>New passphrase</source>
-        <translation>New passphrase</translation>
+        <translation>Nowe hasło</translation>
     </message>
     <message>
         <location line="+14"/>
         <source>Repeat new passphrase</source>
-        <translation>Repeat new passphrase</translation>
+        <translation>Powtórz nowe hasło</translation>
     </message>
     <message>
         <location line="+33"/>
         <source>Serves to disable the trivial sendmoney when OS account compromised. Provides no real security.</source>
-        <translation>Serves to disable the trivial sendmoney when OS account compromised. Provides no real security.</translation>
+        <translation>Służy do wyłączenia wysyłania pieniędzy, kiedy konto systemowe może być podatne na atak. Brak rzeczywistego bezpieczeństwa.</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>For anonymization and staking only</source>
-        <translation>For anonymization and staking only</translation>
+        <translation>Tylko do anonimizacji i staking </translation>
     </message>
     <message>
         <location filename="../askpassphrasedialog.cpp" line="+42"/>
